bcdd33edc64861308f16f517c260e141df1ad2db
ErenDuzel <duzel.eren96@gmail.com>
2023-04-14 13:45:48 +0300

    EndPoints class created

---
+++ AuthMicroService/src/main/java/com/group/constants/EndPoints.java
@@ -0,0 +1,22 @@
+package com.group.constants;
+
+public class EndPoints {
+    /**
+     *
+     */
+
+
+    public static final String VERSION = "/v1";
+    public static final String DEV = "/dev";
+    public static final String API = "/api";
+
+    public static final String AUTH = "/auth";
+    public static final String SATIS = "/satis";
+    public static final String URUN = "/urun";
+    public static final String SAVE = "/save";
+    public static final String UPDATE = "/update";
+    public static final String DELETE = "/delete";
+    public static final String FINDALL = "/getall";
+    public static final String LOGIN = "/login";
+    public static final String REGISTER = "/register";
+}
