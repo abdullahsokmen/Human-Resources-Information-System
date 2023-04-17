fcf27699cc1ac69eeb0809580ef811f6a1389f6d
brtacer <beratacer125@gmail.com>
2023-04-17 22:20:15 +0300

    fix mailservice

--- AuthMicroService/build/generated/sources/annotationProcessor/java/main/com/group/mapper/IAuthMapperImpl.java
+++ AuthMicroService/build/generated/sources/annotationProcessor/java/main/com/group/mapper/IAuthMapperImpl.java
@@ -1,6 +1,7 @@
 package com.group.mapper;
 
 import com.group.dto.request.RegisterRequestDto;
+import com.group.dto.request.SaveRequestDto;
 import com.group.dto.response.FindByIdResponseDto;
 import com.group.repository.entity.Auth;
 import javax.annotation.processing.Generated;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-04-14T15:01:11+0300",
+    date = "2023-04-17T14:32:59+0300",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.4.jar, environment: Java 17.0.5 (Amazon.com Inc.)"
 )
 @Component
@@ -23,7 +24,7 @@ public class IAuthMapperImpl implements IAuthMapper {
         Auth.AuthBuilder<?, ?> auth = Auth.builder();
 
         auth.name( dto.getName() );
-        auth.mail( dto.getMail() );
+        auth.email( dto.getEmail() );
         auth.surname( dto.getSurname() );
         auth.password( dto.getPassword() );
 
@@ -40,8 +41,24 @@ public class IAuthMapperImpl implements IAuthMapper {
 
         findByIdResponseDto.name( auth.getName() );
         findByIdResponseDto.surname( auth.getSurname() );
+        findByIdResponseDto.email( auth.getEmail() );
         findByIdResponseDto.phone( auth.getPhone() );
 
         return findByIdResponseDto.build();
+    }
+
+    @Override
+    public SaveRequestDto toSaveRequestDto(Auth auth) {
+        if ( auth == null ) {
+            return null;
+        }
+
+        SaveRequestDto.SaveRequestDtoBuilder saveRequestDto = SaveRequestDto.builder();
+
+        saveRequestDto.name( auth.getName() );
+        saveRequestDto.surname( auth.getSurname() );
+        saveRequestDto.password( auth.getPassword() );
+
+        return saveRequestDto.build();
     }
 }
