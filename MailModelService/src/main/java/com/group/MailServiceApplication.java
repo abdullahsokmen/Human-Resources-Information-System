e60c4f97172dab2e554cc5e286d0b1b9bd3449df
brtacer <beratacer125@gmail.com>
2023-04-18 11:15:17 +0300

    mail

--- MailModelService/src/main/java/com/group/MailServiceApplication.java
+++ MailModelService/src/main/java/com/group/MailServiceApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MailServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MailServiceApplication.class);
+        //tekrar
     }
 }
