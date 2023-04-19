ccf41a279da4024f41bff09451e664eaa5799545
ErenDuzel <duzel.eren96@gmail.com>
2023-04-19 12:52:17 +0300

    config client dependencies added

--- AdminMicroService/src/main/java/com/group/exception/EErrorType.java
+++ AdminMicroService/src/main/java/com/group/exception/EErrorType.java
@@ -12,13 +12,13 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @Getter
 public enum EErrorType {
 
-    REGISTER_ERROR_PASSWORD_UNMATCH(1004,"Girilen parolalar uyusmamaktadir", BAD_REQUEST),
-    REGISTER_ERROR_USERNAME(1005,"Bu kullanıcı adı daha önce alınmıştır", BAD_REQUEST),
-    LOGIN_ERROR_USERNAME_PASSWORD(1006,"Kullanıcı adı yada şifre hatalıdır", BAD_REQUEST),
-    METHOD_MIS_MATCH_ERROR(2002,"Giriş yaptığınız değer, istenilen değerle uyuşmamaktadır", BAD_REQUEST),
-    METHOD_NOT_VALID_ARGUMENT_ERROR(2003,"URL içinde eksik parametre gönderimi",BAD_REQUEST),
-    INVALID_TOKEN(4114,"Geçersiz token",HttpStatus.BAD_REQUEST),
-    INVALID_PARAMETER(3001,"Geçersiz parametre giriş yaptınız", BAD_REQUEST),
+    REGISTER_ERROR_PASSWORD_UNMATCH(1004,"Passwords not matched", BAD_REQUEST),
+    REGISTER_ERROR_USERNAME(1005,"This Username already taken", BAD_REQUEST),
+    LOGIN_ERROR_USERNAME_PASSWORD(1006,"Username or Password are incorrect", BAD_REQUEST),
+    METHOD_MIS_MATCH_ERROR(2002,"The value you entered does not match the desired value.", BAD_REQUEST),
+    METHOD_NOT_VALID_ARGUMENT_ERROR(2003,"Missing parameter submission in URL",BAD_REQUEST),
+    INVALID_TOKEN(4114,"Invalid token",HttpStatus.BAD_REQUEST),
+    INVALID_PARAMETER(3001,"Invalid parameter entered", BAD_REQUEST),
     USER_NOT_FOUND(3001,"User not found", BAD_REQUEST),
     ADMIN_NOT_FOUND(3002,"Admin not found", BAD_REQUEST)
 
--- AuthMicroService/src/main/java/com/group/exception/EErrorType.java
+++ AuthMicroService/src/main/java/com/group/exception/EErrorType.java
@@ -13,19 +13,19 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 @Getter
 public enum EErrorType {
 
-    REGISTER_ERROR_PASSWORD_UNMATCH(1004,"Girilen parolalar uyusmamaktadir", BAD_REQUEST),
-    REGISTER_ERROR_USERNAME(1005,"Bu kullanıcı adı daha önce alınmıştır", BAD_REQUEST),
-    LOGIN_ERROR_USERNAME_PASSWORD(1006,"Kullanıcı adı yada şifre hatalıdır", BAD_REQUEST),
-    METHOD_MIS_MATCH_ERROR(2002,"Giriş yaptığınız değer, istenilen değerle uyuşmamaktadır", BAD_REQUEST),
-    METHOD_NOT_VALID_ARGUMENT_ERROR(2003,"URL içinde eksik parametre gönderimi",BAD_REQUEST),
-    INVALID_PARAMETER(3001,"Geçersiz parametre giriş yaptınız", BAD_REQUEST),
+    REGISTER_ERROR_PASSWORD_UNMATCH(1004,"Passwords not matched", BAD_REQUEST),
+    REGISTER_ERROR_USERNAME(1005,"This Username already taken", BAD_REQUEST),
+    LOGIN_ERROR_USERNAME_PASSWORD(1006,"Username or Password are incorrect", BAD_REQUEST),
+    METHOD_MIS_MATCH_ERROR(2002,"The value you entered does not match the desired value.", BAD_REQUEST),
+    METHOD_NOT_VALID_ARGUMENT_ERROR(2003,"Missing parameter submission in URL",BAD_REQUEST),
+    INVALID_PARAMETER(3001,"Invalid parameter entered", BAD_REQUEST),
     USER_NOT_FOUND(3001,"User not found", BAD_REQUEST),
-    INVALID_TOKEN(3002,"Geçersiz token",HttpStatus.BAD_REQUEST),
+    INVALID_TOKEN(3002,"Invalid token",HttpStatus.BAD_REQUEST),
 
     EMAIL_ALREADY_TAKEN(3001,"User not found", BAD_REQUEST),
     PHONE_ALREADY_TAKEN(3001,"User not found", BAD_REQUEST),
 
-    ACTIVATE_CODE_ERROR(4113,"Aktivasyon kod hatası",HttpStatus.BAD_REQUEST),
+    ACTIVATE_CODE_ERROR(4113,"Activation Code Error",HttpStatus.BAD_REQUEST),
 
 
     ;
