fc1c5834e4c061bb9c22d56f54c370f908371c2e
abdullahsokmen <Abdu11ahsk@icloud.com>
2023-04-24 15:23:36 +0300

    Validations added

--- AdminMicroService/src/main/java/com/group/dto/request/EditProfileRequestDto.java
+++ AdminMicroService/src/main/java/com/group/dto/request/EditProfileRequestDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @Data
@@ -13,8 +16,13 @@ import java.util.Date;
 @NoArgsConstructor
 public class EditProfileRequestDto {
     private Long id;
+    @Pattern(regexp = "^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$")
     private Date birthDate;
     private String birthPlace;
+    @Pattern(regexp = "^[1-9]{1}[0-9]{9}[02468]{1}$")
+    @NotBlank(message = "Identify can not be blank")
     private String identity;
+    @Pattern(regexp ="/^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$/")
+    @NotNull(message = "Phone can not be blank")
     private String phone;
 }
--- CompanyMicroService/src/main/java/com/group/dto/request/CompanySaveRequestDto.java
+++ CompanyMicroService/src/main/java/com/group/dto/request/CompanySaveRequestDto.java
@@ -5,20 +5,36 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanySaveRequestDto {
-
+    @NotBlank(message = "Company Name can not be blank")
     private String companyName;
+    @NotBlank(message = "Title can not be blank")
     private String title;
+    @NotBlank(message = "Mersis No can not be blank")
     private String mersisNo;
+    @NotBlank(message = "Tax No can not be blank")
     private String taxNo;
+    @NotBlank(message = "Tax Office name can not be blank")
     private String taxOffice;
+    @NotBlank(message = "Logo Url can not be blank")
     private String logoUrl;
+    @NotBlank(message = "Phone can not be blank")
+    @Pattern(regexp ="/^(05)([0-9]{2})\\s?([0-9]{3})\\s?([0-9]{2})\\s?([0-9]{2})$/")
     private String phone;
+    @NotBlank(message = "Address can not be blank")
     private String address;
+    @NotBlank(message = "Email can not be blank")
+    @Email
     private String email;
+    @NotBlank(message = "Foundation Year can not be blank")
     private String foundationYear;
 }
