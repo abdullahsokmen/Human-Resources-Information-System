16f906c3b192ae7212d6445fdf59b4a4e81a5a45
abdullahsokmen <Abdu11ahsk@icloud.com>
2023-04-25 12:08:58 +0300

    CompanyAdmin Micro Service

---
+++ CompanyAdminMicroService/src/main/java/com/group/dto/request/RegisterRequestDto.java
@@ -0,0 +1,17 @@
+package com.group.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequestDto {
+    private String name;
+    private String lastname;
+    private String email;
+    private String phone;
+}
