c5d6cfa3951618f67881008df6295c901ea4c9ae
brtacer <beratacer125@gmail.com>
2023-04-26 11:59:13 +0300

    add dto

---
+++ PersonalMicroService/src/main/java/com/group/repository/entity/Address.java
@@ -0,0 +1,26 @@
+package com.group.repository.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tbladdress")
+public class Address extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String town;
+    private String city;
+    private String country;
+    private String description;
+    @OneToOne
+    private Personal personal;
+}
--- PersonalMicroService/src/main/java/com/group/service/PersonalService.java
+++ PersonalMicroService/src/main/java/com/group/service/PersonalService.java
@@ -1,8 +1,10 @@
 package com.group.service;
 
+import com.group.dto.request.PersonalSaveRequestDto;
 import com.group.repository.IPersonalRepository;
 import com.group.repository.entity.Personal;
 import com.group.utility.ServiceManager;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,5 +14,9 @@ public class PersonalService extends ServiceManager<Personal,Long> {
     public PersonalService(IPersonalRepository personalService) {
         super(personalService);
         this.personalService = personalService;
+    }
+
+    public Boolean createPersonal(PersonalSaveRequestDto dto) {
+
     }
 }
