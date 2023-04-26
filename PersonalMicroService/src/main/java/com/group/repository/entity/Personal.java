45b1f060b04eb4999b1736f2772331c967af9e6e
ErenDuzel <duzel.eren96@gmail.com>
2023-04-26 11:46:13 +0300

    Personal Entity created

--- PersonalMicroService/src/main/java/com/group/repository/entity/Personal.java
+++ PersonalMicroService/src/main/java/com/group/repository/entity/Personal.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Data
 @Builder
@@ -17,5 +18,24 @@ public class Personal extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
+    private Long id;
+    private String password;
+    private String photoUrl;
+    private String name;
+    private String secondName;
+    private String lastname;
+    private Date birthDate;
+    private String birthPlace;
+    private String identity;
+    private Date dateOfStart;
+    private Date dateOfEnd;
+    private String major;
+    private String department;
+    private String email;
+    private String phone;
+    private String companyId;
+    private EStatus status = EStatus.ACTIVE;
+
+
+
 }
