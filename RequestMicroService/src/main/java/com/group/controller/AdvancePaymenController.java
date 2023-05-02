eeeafd36c74a574ca22323a4e6c660751679f6bf
abdullahsokmen <Abdu11ahsk@icloud.com>
2023-05-02 13:35:09 +0300

    RequestService update's

---
+++ RequestMicroService/src/main/java/com/group/controller/AdvancePaymenController.java
@@ -0,0 +1,13 @@
+package com.group.controller;
+
+import com.group.service.AdvancePaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import static com.group.constants.EndPoints.*;
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(ADVANCEPAYMENT)
+public class AdvancePaymenController {
+    private final AdvancePaymentService advancePaymentService;
+}
